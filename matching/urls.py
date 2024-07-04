73c4766e2f6fdad5cea8b9333a863f84117631ed
Arman <arman1381keshazar@gmail.com>
2024-07-05 02:34:20 +0330

    token is str in url

--- matching/urls.py
+++ matching/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from matching import views
 
 urlpatterns = [
-    path("getVerifiers/<int:post_token>", views.GetVerifiersView.as_view(), name="get_verifiers"),
+    path("getVerifiers/<str:post_token>", views.GetVerifiersView.as_view(), name="get_verifiers"),
     path("verifier/", views.VerifierView.as_view(), name="verifier"),
-    path("setVerifiers/<int:post_token>", views.SetVerifiersView.as_view(), name="set_verifiers"),
+    path("setVerifiers/<str:post_token>", views.SetVerifiersView.as_view(), name="set_verifiers"),
 ]
