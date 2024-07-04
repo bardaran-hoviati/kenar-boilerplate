5bd0037cf34132f17f114f517fdb9372ba93c98b
sepehrmnp <sepehr.mirnasrollahi@aut.ac.ir>
2024-07-05 00:59:42 +0330

    added test oauth

--- matching/urls.py
+++ matching/urls.py
@@ -4,5 +4,7 @@ from matching import views
 
 urlpatterns = [
     path("getVerifiers/<int:post_token>", views.GetVerifiersView.as_view(), name="get_verifiers"),
-    path("setVerifiers/<int:post_token>", views.SetVerifiersView.as_view(), name="set_verifiers")
+    path("setVerifiers/<int:post_token>", views.SetVerifiersView.as_view(), name="set_verifiers"),
+    path("setVerifier/<int:post_token>/<int:verifier_id>", views.SetVerifierView.as_view(), name="set_verifier"),
+    path("setVerifierOauth", views.SetVerifierOauthView.as_view(), name="set_verifier_oauth")
 ]
