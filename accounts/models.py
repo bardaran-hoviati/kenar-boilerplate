0973a178524c903cf8250e15f968c6e8b0559567
MEgooneh <mohammad.erfan.gooneh@gmail.com>
2024-07-05 09:54:05 +0330

    make migrations for photo field

--- accounts/models.py
+++ accounts/models.py
@@ -27,8 +27,8 @@ class Verifier(TimestampModel):
     phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
     rate = models.DecimalField(decimal_places=2, default=5.0, max_digits=12)
     transactions_participated_count = models.IntegerField(default=0)
-    profile_photo = models.CharField(max_length=255)
-    
+    profile_photo = models.CharField(max_length=255, null=True)
+
     def __str__(self):
         return f'Verifier: {self.firstname} {self.lastname}'
 
